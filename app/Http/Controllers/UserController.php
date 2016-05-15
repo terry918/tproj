29e750063991a4999c282c47487b722d7c2a09e2
Terry918 <terry800918@gmail.com>
2016-05-15 18:17:07 +0800

    hahahahah

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use DB;
+use App\Http\Controllers\Controller;
+use Cache;
+
+class UserController extends Controller{
+    /**
+     * 显示用户列表
+     * @param int $id
+     * @return Response
+     */
+    public function index($id)
+    {
+        //return phpinfo();
+        //$cities = DB::table('pa_cities')->get();
+		//print_r($cities);
+        
+        $arr = array("北京","上海","成都","广州","重庆");
+
+        Cache::put('terry2',$arr,'100');   
+
+        $cities = array('shanghai','beijing','tianjin');
+        return view('user.index', ['cities' => $cities]);
+    }
+}
---
+++ resources/views/user/index.blade.php
@@ -0,0 +1,11 @@
+<h1>Terry 太牛逼了</h1>
+
+<?
+print_r(array_slice($cities,0,5));
+?>
+
+<form name="abc" method="post">
+	<input name="aaa" type="text" value="1" />
+	<input name="bbb" type="text" value="2" />
+	<input name="ccc" type="submit" value="Submit" />
+</form>
